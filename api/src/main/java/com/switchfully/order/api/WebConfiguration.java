1e15117b962581fede1eee82d4091fe3f758e4dd
Klaas Devits <klaas.devits@gmail.com>
2021-12-02 09:32:13 +0100

    added WebConfiguration corsMapping for localhost:4200

---
+++ api/src/main/java/com/switchfully/order/api/WebConfiguration.java
@@ -0,0 +1,17 @@
+package com.switchfully.order.api;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/localhost:4200/**")
+                .allowedMethods("*");
+    }
+
+
+}
